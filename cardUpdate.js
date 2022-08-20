de129f3be07bdfe61804d81d56f7c6202e70f28e
JunoField <junofield123@gmail.com>
2022-08-20 15:22:48 +0100

    added code to prevent letters in exp month

--- cardUpdate.js
+++ cardUpdate.js
@@ -8,3 +8,24 @@ function updateCardImg(fieldName){
         document.getElementById("card-img-" + fieldName).innerHTML = document.getElementById(fieldName + "-input").value;
     }
 }
+
+function updateCardNumber(){
+    var foo = (document.getElementById("number-input").value).split(" ").join(""); // remove hyphens
+    if (foo.length > 0) {
+      foo = foo.match(new RegExp('.{1,4}', 'g')).join(" ");
+    }
+    document.getElementById("number-input").value = foo;
+
+    updateCardImg("number");
+}
+
+function updateExpiryMonth(){
+    var month = document.getElementById("expiry-month-input").value;
+    if (isNaN(month.charAt(month.length - 1))){
+        document.getElementById("expiry-month-input").value = month.slice(0, month.length - 1);
+    } else{
+        if (month == ""){
+
+        }
+    }
+}
