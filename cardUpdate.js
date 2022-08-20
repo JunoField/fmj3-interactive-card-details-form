810cc8f5491570b285462d8f1d9dce86c07f5eff
JunoField <junofield123@gmail.com>
2022-08-20 15:51:09 +0100

    done pre-submission validation

--- cardUpdate.js
+++ cardUpdate.js
@@ -2,30 +2,64 @@
 
 function updateCardImg(fieldName){
     if (fieldName == "date"){
-        let date = document.getElementById("expiry-month-input").value + "/" + document.getElementById("expiry-year-input").value;
+        let date = document.getElementById("expiry-month-input").value.padStart(2, '0') + "/" + document.getElementById("expiry-year-input").value;
         document.getElementById("card-img-date").innerHTML = date;
     } else{
         document.getElementById("card-img-" + fieldName).innerHTML = document.getElementById(fieldName + "-input").value;
     }
 }
 
-function updateCardNumber(){
-    var foo = (document.getElementById("number-input").value).split(" ").join(""); // remove hyphens
-    if (foo.length > 0) {
-      foo = foo.match(new RegExp('.{1,4}', 'g')).join(" ");
+//remove letters from a number string
+function removeTypedLetters(str){
+    if (isNaN(str.charAt(str.length - 1))){
+        str = str.slice(0, str.length - 1);
     }
-    document.getElementById("number-input").value = foo;
+    return str;
+}
+
+
+function updateName(){
+    updateCardImg("name");
+}
+
+function updateCardNumber(){
+    var cardNo = removeTypedLetters(document.getElementById("number-input").value)
+    cardNo = cardNo.split(" ").join(""); // remove hyphens
+    if (cardNo.length > 0) {
+      cardNo = cardNo.match(new RegExp('.{1,4}', 'g')).join(" ");
+    }
+    document.getElementById("number-input").value = cardNo;
 
     updateCardImg("number");
 }
 
 function updateExpiryMonth(){
     var month = document.getElementById("expiry-month-input").value;
-    if (isNaN(month.charAt(month.length - 1))){
-        document.getElementById("expiry-month-input").value = month.slice(0, month.length - 1);
-    } else{
-        if (month == ""){
 
-        }
+    month = removeTypedLetters(month);
+
+    
+    if ((parseInt(month) >= 10 && parseInt(month) <= 12) || (parseInt(month) >= 3)){ //if month is certainly fully entered:
+        document.getElementById("expiry-year-input").focus(); //move onto year
     }
+
+    document.getElementById("expiry-month-input").value = month;
+    updateCardImg("date");
+}
+
+function updateExpiryYear(){
+    var year = document.getElementById("expiry-year-input").value;
+
+    year = removeTypedLetters(year);
+
+
+    document.getElementById("expiry-year-input").value = year;
+    updateCardImg("date");
+}
+
+function updateCVC(){
+    var cvc = document.getElementById("cvc-input").value;
+    cvc = removeTypedLetters(cvc);
+    document.getElementById("cvc-input").value = cvc;
+    updateCardImg("cvc");
 }
